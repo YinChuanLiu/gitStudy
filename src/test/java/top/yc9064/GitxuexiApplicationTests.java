daaf65daa15863226e5febb0794855e0ffbe6d90
yc9064 <yc906478@163.com>
2021-04-15 21:28:45 +0800

    xuexi提交1

---
+++ src/test/java/top/yc9064/GitxuexiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.yc9064;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitxuexiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
